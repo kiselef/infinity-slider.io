27d7c6865cf1deb9543a3f55475c8b4ee7d567ea
mkiselev <arz.kiselev@yandex.ru>
2020-05-03 22:07:33 +0300

    diagonal strategy added

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ window.onload = () => {
   for (let arrow of document.getElementsByClassName('arrow')) {
     arrow.onclick = arrow.className.indexOf('left') !== -1 ? left : right;
   }
-  document.getElementsByClassName('add-slides').item(0).onclick = () => {
-    slider.addDataUrls(Helper.generateUrls());
-  };
+  // document.getElementsByClassName('add-slides').item(0).onclick = () => {
+  //   slider.addDataUrls(Helper.generateUrls());
+  // };
 };
 
 
